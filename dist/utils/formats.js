077bd539a6e204f109967a6c2d944090cbbd69a1
enochndika <enochbeloved@gmail.com>
2021-10-04 01:29:23 +0100

    remove dist folder

--- dist/utils/formats.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.toNumber = void 0;
-function toNumber(value) {
-    return parseInt(String(value), 10);
-}
-exports.toNumber = toNumber;
-//# sourceMappingURL=formats.js.map
