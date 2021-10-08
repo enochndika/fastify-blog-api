e2a4670579d9c090ff810ace642160761f0d6026
enochndika <enochbeloved@gmail.com>
2021-10-08 02:17:25 +0100

    connect to remote db and fix errors

--- src/app.server.ts
+++ src/app.server.ts
@@ -5,7 +5,7 @@ const server: IFastify = main({
   logger: true,
 });
 
-server.listen(process.env.PORT || 3000, (err) => {
+server.listen(process.env.PORT || 80, (err) => {
   if (err) {
     console.log(err);
     process.exit(1);
